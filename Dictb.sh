a12044412a5eb565be0d8d356c72a10f6a79e1f2
Devika18 <52191935+Devika18@users.noreply.github.com>
2022-09-14 09:53:00 +0530

    Update Dictb.sh

--- Dictb.sh
+++ Dictb.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
+c=0
 
-declare -A die
-die[roll1]=1
-die[roll2]=2
-die[roll3]=3
-die[roll4]=4
-die[roll5]=5
-die[roll6]=6
-
-#echo Result after rolling die 6 times ${die[@]}
-#echo Rolling a die for 1st time is: ${die[roll1]}
-#echo Rolling a die for 2nd time is: ${die[roll2]}
-#echo Rolling a die for 3rd time is: ${die[roll3]}
-#echo Rolling a die for 4th time is: ${die[roll4]}
-#echo Rolling a die for 5th time is: ${die[roll5]}
-#echo Rolling a die for 6th time is: ${die[roll6]}
-
-while [[ $die -le 6 ]]
+while [[ $c -le 6 ]]
 do
-echo "Repeating die: "${die[@]}
+die=$((RANDOM%6+1))
+echo "Repeating die: "$die
+((c++))
 done
